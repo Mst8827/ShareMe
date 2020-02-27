839d4134110210d266f2da54dda4e7c4605b4208
Mst8827 <misato27.t@gmail.com>
2020-02-27 16:55:21 +0900

    all_finctions_done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,9 @@
 class PostsController < ApplicationController
-  before_action :move_to_index, except: [:index, :show]
+  before_action :set_post, only: [:edit, :show]
+  before_action :move_to_index, except: [:index, :show, :search]
 
   def index
-    @posts = Post.all
+    @posts = Post.includes(:user)
   end
 
   def new
@@ -21,7 +22,6 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
@@ -30,9 +30,22 @@ class PostsController < ApplicationController
     redirect_to root_path
   end
 
+  def show
+    @comment = Comment.new
+    @comments = @post.comments.includes(:user)
+  end
+
+  def search
+    @posts = Post.search(params[:keyword])
+  end
+
   private
   def post_params
     params.require(:post).permit(:text, :image, :movie).merge(user_id: current_user.id)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 
   def move_to_index
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,18 @@
 class Post < ApplicationRecord
   validates :text, :image, presence: true
   belongs_to :user
+  has_many :comments
+
+  def self.search(search)
+    return Post.all unless search
+    Post.where('text LIKE(?)', "%#{search}%")
+  end
+
+  # 以下のような書き方も出来る
+  # def self.search(search)
+  #   if search
+  #     Post.where('text LIKE(?)', "%#{search}%")
+  #   else
+  #     Post.all
+  #   end
 end
