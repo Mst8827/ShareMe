679ef406c2aa00c88ca40bb86d82bf4d0b25c7be
Mst8827 <misato27.t@gmail.com>
2020-04-02 14:23:23 +0900

    index_and_users_show_pagenation_done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,8 +3,7 @@ class PostsController < ApplicationController
   before_action :move_to_index, except: [:index, :show, :search]
 
   def index
-    @posts = Post.includes(:user)
-    @posts = Post.order("id DESC")
+    @posts = Post.includes(:user).order("created_at DESC").page(params[:page]).per(5)
   end
 
   def new
