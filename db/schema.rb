58243b4a4fc71ee1ba9bcb100bf1f89344067319
Mst8827 <misato27.t@gmail.com>
2020-04-03 16:46:41 +0900

    removed_column_movie_from_posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_29_141358) do
+ActiveRecord::Schema.define(version: 2020_04_03_074001) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
@@ -30,7 +30,6 @@ ActiveRecord::Schema.define(version: 2020_03_29_141358) do
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "text"
     t.string "image"
-    t.string "movie"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
