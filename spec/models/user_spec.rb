fc5a56364628675eae9c3a54693be47291a99727
Mst8827 <misato27.t@gmail.com>
2020-04-09 11:30:49 +0900

    devise_translated_into_JP

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,7 @@ describe User do
     it "nicknameがない場合は登録できないこと" do
      user = User.new(nickname: "", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000")
      user.valid?
-     expect(user.errors[:nickname]).to include("can't be blank")
+     expect(user.errors[:nickname]).to include("を入力してください")
     end
   end
 end
